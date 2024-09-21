1e323962304e04cb36e5f11eb48ecb1c78b697fe
Anton <electroautomatic@gmail.com>
2024-09-21 19:41:13 +0500

    from Clode

--- version/app.js
+++ version/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-app = express();
+const app = express();
 
-var response;
+console.log('Starting application...');
 
 app.get('/', function (req, res) {
-
-    response = 'This is version 3 of the app.' + '\n';
-
-    //send the response to the client
+    console.log('Received request at /');
+    const response = 'This is version 3 of the app.\n';
     res.send(response);
-
 });
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+app.listen(8080, '0.0.0.0', function (err) {
+    if (err) {
+        console.error('Error starting server:', err);
+    } else {
+        console.log('Server listening on 0.0.0.0:8080');
+    }
 });
 
+// Добавим обработку необработанных ошибок
+process.on('uncaughtException', function(err) {
+    console.error('Uncaught exception:', err);
+});
