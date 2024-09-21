e4a9f263be26ed218a0800b4ecba430fd56ef228
Anton <electroautomatic@gmail.com>
2024-09-21 18:24:16 +0500

    test

--- version/app.js
+++ version/app.js
@@ -1,17 +1,29 @@
-const express = require('express');
-app = express();
+// const express = require('express');
+// app = express();
 
-var response;
+// var response;
 
+// app.get('/', function (req, res) {
+
+//     response = 'This is version 3 of the app.' + '\n';
+
+//     //send the response to the client
+//     res.send(response);
+
+// });
+
+// app.listen(8080, function () {
+//   console.log('Server listening on port 8080...');
+// });
+
+var express = require('express');
+var app = express();
 app.get('/', function (req, res) {
-
-    response = 'This is version 3 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
+  res.send('Hello World!');
 });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
-app.listen(8080, function () {
-  console.log('Server listening on port 8080...');
+app.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
